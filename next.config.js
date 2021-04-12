fb891b7def55d2851f258927908b198eb7c4fc95
Mainul Dip <mainuldip@gmail.com>
2021-04-12 22:27:59 +0600

    Pushing gh-pages

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-// git push origin --delete gh-pages
-// git subtree push --prefix out origin gh-pages
-module.exports = {
-  basePath: isProd ? '/NextjsPageTransition' : '',
-  assetPrefix: isProd ? '/NextjsPageTransition' : '',
-}
